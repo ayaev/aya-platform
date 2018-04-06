de55c4bd357aefaad66005298592fc3a5fdb9bb3
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-04-06 12:35:08 +0200

    Update vehicle-editor.js

--- events/scripts/vehicle-editor.js
+++ events/scripts/vehicle-editor.js
@@ -1,11 +1,20 @@
-/**********
+/*
+ * Copyright 2016-2018 Martin Baranski, TroubleZone.Net Productions
+ *
+ * Licensed under the EUPL, Version 1.2 only (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ *
  * File:    vehicle-editor.js - functions for AYA vehicle editor
  * Version: 2.32
  * Date:    2018-01-30
- * Author:  Martin Baranski, TroubleZone.Net Productions
- * Licence: Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
- *          (see: https://creativecommons.org/licenses/by-sa/4.0/ for details)
- **********/
+ */
 
 var color = $('#vehicle-color');
 var components = $('#vehicle-components');
