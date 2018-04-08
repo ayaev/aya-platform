83513bd45dbe98cac24757d484f5cca075769cc6
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-04-08 18:39:36 +0200

    Fix #5 & #6

---
+++ juror.php
@@ -0,0 +1,145 @@
+<?php
+/*
+ * Copyright 2016-2018 Martin Baranski, TroubleZone.Net Productions
+ *
+ * Licensed under the EUPL, Version 1.2 only (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ *
+ * File:    juror.php - AYA juror page
+ * Version: 1.13
+ * Date:    2018-04-07
+ */
+
+require_once('db-initialization.php');
+
+if (!$isJuror)
+{
+  header('Location: /events/listing.php', true, 303);
+  die('Only jurors beyond this point! Sorry.');
+}
+
+$title = 'AYA — Jurorenbereich 1.5';
+require_once('fragments/header.php');
+
+$showDistance = false;
+$showDistanceShortened = false;
+$showListingLink = true;
+$showMap = false;
+require_once('fragments/navigation.php');
+
+try
+{
+  $event = $db->prepare('SELECT EventID, Name, Date
+                         FROM aya_events
+                         WHERE Deleted = FALSE
+                           AND DATEDIFF(Date, CURDATE()) < 6
+                         ORDER BY Date DESC
+                         LIMIT 1');
+  $event->execute();
+  $ayaEvent = $event->fetch(PDO::FETCH_ASSOC);
+  $event = null;
+}
+catch (PDOException $exception)
+{
+  print 'Error: ' . $exception->getMessage() . '<br />';
+}
+?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class="panel panel-aya">
+        <div class="panel-heading">
+          <span class="glyphicon glyphicon-minus-sign "></span> Einbaumängel — <?=$ayaEvent['Name'] . ' ('
+            . date('d.m.Y, H:i', strtotime($ayaEvent['Date'])) . ')';?>
+        </div>
+        <div class="panel-body panel-scrollable-xl">
+          <div class="table-responsive">
+            <table id="flaws" class="table table-hover">
+              <thead>
+                <tr>
+                  <th class="text-center"><span class="glyphicon glyphicon-check"></span></th>
+                  <th class="text-center">Klasse</th>
+                  <th class="text-center">Realname</th>
+                  <th class="text-center">Kennzeichen</th>
+                  <th class="text-center">Fahrzeug</th>
+                  <th class="text-center">Mängelfrei</th>
+                </tr>
+              </thead>
+              <tbody>
+<?php
+try
+{
+  $vehicles = $db->prepare("SELECT C.ClassID, A.phpBBUserID, A.VehicleID, C.Name AS ClassName,
+                            CONCAT(P.pf_vor_nachname_, ', ', P.pf_vorname) AS RealName, V.RegistrationNumber,
+                            CONCAT(M.Name, ' ', V.Model) AS VehicleName,
+                            CASE WHEN LENGTH(V.InstallFlaws) > 0 THEN true ELSE false END AS IsFlawed,
+                            CHAR_LENGTH(V.InstallFlaws) AS Flawed
+                            FROM aya_attendees A
+                            JOIN aya_classes C
+                              ON A.ClassID = C.ClassID
+                            JOIN phpbb_profile_fields_data P
+                              ON A.phpBBUserID = P.user_id
+                            JOIN aya_vehicles V
+                              ON A.VehicleID = V.VehicleID
+                            JOIN aya_vehicles_manufacturers M
+                              ON V.ManufacturerID = M.ManufacturerID
+                            JOIN aya_events E
+                              ON A.EventID = E.EventID
+                            WHERE A.Deleted = FALSE
+                              AND A.EventID = :id
+                              AND DATEDIFF(E.Date, CURDATE()) < 6
+                            ORDER BY C.SortKey ASC, RealName ASC");
+  $vehicles->bindValue(':id', $ayaEvent['EventID'], PDO::PARAM_INT);
+  $vehicles->execute();
+
+  while ($vehicle = $vehicles->fetch(PDO::FETCH_ASSOC))
+  {
+    echo '<tr class="alert-' . ($vehicle['IsFlawed'] ? 'danger' : 'success') . '">
+            <td class="text-center">
+              <input data-class-id="' . $vehicle['ClassID'] . '" data-user-id="' . $vehicle['phpBBUserID'] . '" data-vehicle-id="'
+                . $vehicle['VehicleID'] . '" type="checkbox" />
+            </td>
+            <td class="text-center">' . $vehicle['ClassName'] . '</td>
+            <td class="text-center">' . $vehicle['RealName'] . '</td>
+            <td class="text-center">' . $vehicle['RegistrationNumber'] . '</td>
+            <td class="text-center">' . $vehicle['VehicleName'] . '</td>
+            <td class="text-center">
+              <span class="glyphicon glyphicon-' . ($vehicle['IsFlawed'] ? 'remove' : 'ok') . '-sign" aria-hidden="true"></span>
+            </td>
+          </tr>';
+  }
+
+  $vehicles = null;
+}
+catch (PDOException $exception)
+{
+  print 'Error: ' . $exception->getMessage() . '<br />';
+}
+?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="panel-footer">
+          <span class="glyphicon glyphicon-list-alt"></span> Optionen:
+          <button id="flaws-update" class="btn btn-aya-default" type="button"><span class="glyphicon glyphicon-edit"></span> Aktualisieren</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
+<?php
+$db = null;
+require_once('fragments/footer.php');
+?>
+</body>
+</html>
---
+++ scripts/bootstrap-dialogs.js
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2016-2018 Martin Baranski, TroubleZone.Net Productions
+ *
+ * Licensed under the EUPL, Version 1.2 only (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ *
+ * File:    bootstrap-dialogs.js - jQuery functions for the Bootstrap dialogs of AYA event detail pages
+ * Version: 3.56
+ * Date:    2018-04-07
+ */
+
+var ajaxFolder = 'ajax/';
+var fragmentsFolder = 'fragments/';
+var scriptsFolder = 'scripts/';
+
+var delete_ = '#delete';
+var editor = '-editor';
+var inputs_ = '-inputs'; // "input" is used by bootstrap-select library
+var fragmentTarget = '#fragment-modal';
+var mode = ''; // TODO: Remove this, but in conjunction with updated vehicle-editor.js (depends on modifying put/post for AJAX save call)
+var result = '#result';
+var save = '#save';
+
+var createLabel = 'Hinzufügen';
+var updateLabel = 'Aktualisieren';
+
+var defaultResponses = {
+  AlreadyDeleted: 'bereits gelöscht',
+  AlreadyExists: 'existiert bereits',
+  MissingData: 'fehlende Angaben',
+  NonExistent: 'existiert nicht',
+  Unchanged: 'keine Änderung',
+  Unknown: 'Unbekannter Fehler'
+};
+
+function attachEventHandler(selector) {
+  $(selector).each(function() {
+    $(this).on('input', function(event) {
+      var initialValue = '';
+      if (event.target.type === "date" || event.target.type === "textarea" || event.target.type === "time") {
+        initialValue = event.target.defaultValue;
+      } else if (event.target.value !== '') {
+        initialValue = event.target.placeholder;
+      }
+      enableSaveOnModified(event.target.value, initialValue);
+    });
+  });
+}
+
+function enableDeleteOnNonDefaults(selector) {
+  // val == 0 -> 'Please select' option
+  // original: $('#selector ').siblings()
+  $(delete_).prop('disabled', ((selector.selectpicker('val') == 0) || (selector.children('option:selected').siblings().length < 3) ? 'disabled' : false));
+}
+
+function enableSaveOnModified(currentValue, initialValue) {
+  $(save).prop('disabled', (currentValue !== initialValue ? false : 'disabled')).text(mode === 'post' ? createLabel : updateLabel);
+}
+
+function enableSelectionByRowClick(panelId) {
+  $(panelId + ' > tbody > tr').click(function(event) {
+    if (event.target.type !== 'checkbox') {
+      $(this).find('td > input:checkbox').prop('checked', function(index, value) {
+        return !value;
+      });
+    }
+  });
+}
+
+function fragmentLoader(fragment, ajaxMode, source, finishCallback) {
+  $(fragmentTarget).load(fragmentsFolder + fragment + editor + '.php',
+    getRequestData(fragment, source),
+    function() {
+      var saveButton = $(save);
+      mode = ajaxMode;
+      if (mode === 'put') {
+        saveButton.text(updateLabel);
+      }
+      $.ajax({
+        dataType: 'script',
+        url: scriptsFolder + fragment + editor + '.js'
+      }).done(function() {
+        if (typeof(loadedCallback) !== 'undefined' && $.isFunction(loadedCallback)) {
+          loadedCallback();
+        }
+      });
+
+      saveButton.click(function() {
+        saveButton.prop('disabled', 'disabled');
+
+        $.ajax({
+          cache: false,
+          data: getPayload(),
+          method: 'POST',
+          url: ajaxFolder + mode + '-' + fragment + '.php'
+        }).done(function(response) {
+          // TODO: Update source parameter (contains panel) with submitted data
+          showMessage(displayName, mode, response, finishCallback); // displayName is defined in each *-editor.js
+          saveButton.prop('disabled', (response > 0));
+        });
+      });
+
+      $('#' + fragment + editor + '-dialog').modal('show');
+    });
+}
+
+function getRequestData(fragment, source) {
+  switch (fragment) {
+    case 'attendance':
+      return {
+        [ 'EventID' ]: source
+      };
+
+    case 'flaw':
+      let flaw = $(source + ' input:checked').first();
+      return {
+        [ 'ClassID' ]: flaw.data('class-id'),
+        [ 'UserID' ]: flaw.data('user-id'),
+        [ 'VehicleID' ]: flaw.data('vehicle-id')
+      };
+
+    default:
+      return {
+        [ fragment.charAt(0).toUpperCase() + fragment.slice(1) + 'ID' ]: $(source + ' input:checked').first().data(fragment + '-id')
+      };
+  }
+}
+
+function hideModal(callback) {
+  setTimeout(function() {
+    $(fragmentTarget + ' > div.modal')
+      .modal('hide')
+      .on('hidden.bs.modal', function(event) {
+        if (callback && typeof(callback) === 'function') {
+          callback();
+        }
+      });
+  }, 2000);
+}
+
+function showMessage(displayName, mode, response, finishCallback) {
+  var resultPanel = $(result);
+  var isSuccess = response > -1;
+  var isUnchanged = response == 0;
+
+  resultPanel.attr('class', 'alert aya-alert-ajax alert-' + (isUnchanged ? 'info' : (isSuccess ? 'success' : 'danger')));
+  var icon = '<span class="glyphicon glyphicon-';
+  var info = '-sign" aria-hidden="true"></span> ';
+  if (isSuccess) {
+    icon += 'ok';
+  } else if (isUnchanged) {
+    icon += 'info';
+    info += 'Hinweis: ';
+  } else {
+    icon += 'exclamation';
+    info += 'Fehler: ';
+  }
+
+  var message = icon + info + displayName + ' ' + (isSuccess && !isUnchanged ? 'erfolgreich' : 'nicht') + ' ';
+  switch (mode) {
+    case 'delete':
+      message += 'gelöscht';
+      break;
+
+    case 'post':
+      message += 'angelegt';
+      break;
+
+    case 'put':
+      message += 'aktualisiert';
+      break;
+
+    default:
+      message += 'möglich';
+      break;
+  }
+
+  if (!isSuccess || isUnchanged)
+  {
+    message += ' (';
+
+    if (typeof(responseCallback) !== 'undefined' && $.isFunction(responseCallback)) {
+      message += responseCallback(response);
+    } else {
+      switch (response) {
+        case '0':
+          message += defaultResponses.Unchanged;
+          break;
+
+        case 'ALREADY_DELETED':
+          message += defaultResponses.AlreadyDeleted;
+          break;
+
+        case 'ALREADY_EXISTS':
+          message += defaultResponses.AlreadyExists;
+          break;
+
+        case 'MISSING_DATA':
+          message += defaultResponses.MissingData;
+          break;
+
+        case 'NON_EXISTENT':
+          message += defaultResponses.NonExistent;
+          break;
+
+        default:
+          message += defaultResponses.Unknown;
+          break;
+      }
+    }
+
+    message += ')';
+  }
+
+  resultPanel.html(message + '.' + (isSuccess && !isUnchanged ? '' : ' Bitte korrigieren!'));
+  resultPanel.slideDown();
+
+  if (isSuccess && !isUnchanged) {
+    hideModal(finishCallback);
+  }
+}
+
+$(document).ready(function() {
+  var attendance = 'attendance';
+
+  $('.' + attendance).each(function() {
+    var that = $(this);
+    that.click(function() {
+      that.prop('disabled', 'disabled');
+      fragmentLoader('attendance', 'post', that.data('event-id'), null);
+      that.prop('disabled', false);
+    });
+  });
+
+  $('#' + attendance + editor).click(function() {
+    fragmentLoader('attendance', 'put', null, null);
+  });
+
+  $('#vehicle' + editor).click(function() {
+    fragmentLoader('vehicle', 'put', null, null);
+  });
+
+  // TODO: Refactor this!
+  $('tbody').each(function() {
+    $(this).on('click', 'tr', function(event) {
+      if (event.target.type === 'checkbox') {
+        return;
+      }
+
+      $(this).find('th > input').prop('checked', function(index, value) {
+        return !value;
+      });
+    });
+  });
+});
